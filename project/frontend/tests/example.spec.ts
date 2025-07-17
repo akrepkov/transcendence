55a9e2b8c13e1dcf32e1ac5727dda1589d14a416
DjoykeReijans <crudendjoyke@gmail.com>
2025-07-17 16:28:13 +0200

    added frontend testing yml

---
+++ project/frontend/tests/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('tests if homepage has correct title and content', async ({ page }) => {
+  await page.goto('https://localhost:3000');
+  await expect(page).toHaveTitle(/Pong Game/i);
+  // Take screenshot optional
+  await page.screenshot({ path: 'homepage-screenshot.png', fullPage: true });
+});
