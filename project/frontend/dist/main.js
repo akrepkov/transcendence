da6b55086b4979ddc3fca8dfb0c5969c81811bb8
DjoykeReijans <crudendjoyke@gmail.com>
2025-07-22 18:10:15 +0200

    cleanup

--- project/frontend/dist/main.js
+++
@@ -1,6 +0,0 @@
-import { handleRegister } from './register/register.js';
-import { handleLogin } from './login/login.js';
-import { toggleForms } from './toggle/toggleForms.js';
-handleRegister();
-handleLogin();
-toggleForms();
