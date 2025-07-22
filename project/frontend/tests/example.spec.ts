942e86b8e6033333b4d8eb04e30cb50eb0d9a1b8
DjoykeReijans <crudendjoyke@gmail.com>
2025-07-22 18:59:12 +0200

    removed taking screenshot in example.spec.ts

--- project/frontend/tests/example.spec.ts
+++ project/frontend/tests/example.spec.ts
@@ -3,6 +3,4 @@ import { test, expect } from '@playwright/test';
 test('tests if homepage has correct title and content', async ({ page }) => {
   await page.goto('https://localhost:3000');
   await expect(page).toHaveTitle(/Pong/i);
-  // Take screenshot optional
-  await page.screenshot({ path: 'homepage-screenshot.png', fullPage: true });
 });
